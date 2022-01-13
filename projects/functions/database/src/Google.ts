b8de6b9e8dd81f4c3ebf77da95be7d58bfd634ce
TGTGamer <jonathan@videndum.studio>
2022-01-13 11:52:55 +0000

    feat: Database working

---
+++ projects/functions/database/src/Google.ts
@@ -0,0 +1,10 @@
+import 'reflect-metadata';
+
+import { ApolloServer } from 'apollo-server-cloud-functions';
+import { options } from './util/helpers';
+
+/**============================================
+ **               Google Handler
+ *=============================================**/
+const google = new ApolloServer({ ...options, playground: true });
+export const handler = google.createHandler();
---
+++ projects/functions/database/src/util/authentication.ts
@@ -0,0 +1,64 @@
+import { Oso } from 'oso';
+import {
+  Account,
+  Address,
+  Application,
+  Domain,
+  Email,
+  Feature,
+  Files,
+  Phone,
+  RTW,
+  Room,
+  Status,
+  Subscription,
+  Transaction,
+} from '@generated/type-graphql';
+import { Context } from './helpers';
+import { AuthCheckerInterface, ResolverData } from 'type-graphql';
+
+export type Resource =
+  | Account
+  | Address
+  | Application
+  | Domain
+  | Email
+  | Feature
+  | Files
+  | Phone
+  | RTW
+  | Room
+  | Status
+  | Subscription
+  | Transaction;
+
+export class CustomAuthChecker implements AuthCheckerInterface<Context> {
+  // inject dependency
+  private oso: Oso;
+  constructor(private readonly resource: Resource) {
+    this.oso = new Oso();
+    this.oso.registerClass(Account);
+    this.oso.registerClass(Address);
+    this.oso.registerClass(Application);
+    this.oso.registerClass(Domain);
+    this.oso.registerClass(Email);
+    this.oso.registerClass(Feature);
+    this.oso.registerClass(Files);
+    this.oso.registerClass(Phone);
+    this.oso.registerClass(RTW);
+    this.oso.registerClass(Room);
+    this.oso.registerClass(Status);
+    this.oso.registerClass(Subscription);
+    this.oso.registerClass(Transaction);
+  }
+
+  async check({ context, info }: ResolverData<Context>, _roles: string[]) {
+    console.log('this ran');
+    await this.oso.loadFiles(['policy.polar']);
+    return await this.oso.isAllowed(
+      context.user,
+      info.operation.operation,
+      this.resource
+    );
+  }
+}
