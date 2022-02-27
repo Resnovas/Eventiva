ae7b108e2103793df16370585a0972b3d3a6483c
TGTGamer <jonathan@resnovas.com>
2022-02-27 11:29:15 +0000

    chore: update license notice

--- projects/functions/siacheck/src/AWS/index.ts
+++ projects/functions/siacheck/src/AWS/index.ts
@@ -1,19 +1,22 @@
 /**
- * File: index.ts
  * Project: @eventiva/siacheck
- * Created Date: Wednesday, February 23rd 2022
+ * File: index.ts
+ * Path: \src\AWS\index.ts
+ * Created Date: Saturday, February 26th 2022
  * Author: Jonathan Stevens
  * -----
- * Last Modified: Fri Feb 25 2022
+ * Last Modified: Sat Feb 26 2022
  * Modified By: Jonathan Stevens
+ * Current Version: 2.0.0
  * -----
- * Copyright (c) 2022 Resnovas
+ * Copyright (c) 2022 Resnovas - All Rights Reserved
  * -----
- * LICENSE: GNU General Public License v3.0 or later
+ * LICENSE: GNU General Public License v3.0 or later (GPL-3.0+)
  *
- * This program has been provided under confidence of the author and is licensed
- * under the terms of the GNU General Public License v3.0 or later as published as
- * version 3 of the License, or (at your option) any later version.
+ * This program has been provided under confidence of the copyright holder and is
+ * licensed for copying, distribution and modification under the terms of
+ * the GNU General Public License v3.0 or later (GPL-3.0+) published as the License,
+ * or (at your option) any later version of this license.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -21,7 +24,10 @@
  * GNU General Public License v3.0 or later for more details.
  *
  * You should have received a copy of the GNU General Public License v3.0 or later
- * along with this program. If not, see http://www.gnu.org/licenses/gpl-3.0-standalone.html
+ * along with this program. If not, please write to: jonathan@resnovas.com ,
+ * or see http://www.gnu.org/licenses/gpl-3.0-standalone.html
+ *
+ * DELETING THIS NOTICE AUTOMATICALLY VOIDS YOUR LICENSE - PLEASE SEE THE LICENSE FILE FOR DETAILS
  * -----
  * HISTORY:
  * Date      	By	Comments
--- projects/functions/siacheck/src/util/routes.ts
+++ projects/functions/siacheck/src/util/routes.ts
@@ -1,3 +1,39 @@
+/**
+ * Project: @eventiva/siacheck
+ * File: routes.ts
+ * Path: \src\util\routes.ts
+ * Created Date: Saturday, February 26th 2022
+ * Author: Jonathan Stevens
+ * -----
+ * Last Modified: Sat Feb 26 2022
+ * Modified By: Jonathan Stevens
+ * Current Version: 2.0.0
+ * -----
+ * Copyright (c) 2022 Resnovas - All Rights Reserved
+ * -----
+ * LICENSE: GNU General Public License v3.0 or later (GPL-3.0+)
+ *
+ * This program has been provided under confidence of the copyright holder and is
+ * licensed for copying, distribution and modification under the terms of
+ * the GNU General Public License v3.0 or later (GPL-3.0+) published as the License,
+ * or (at your option) any later version of this license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License v3.0 or later for more details.
+ *
+ * You should have received a copy of the GNU General Public License v3.0 or later
+ * along with this program. If not, please write to: jonathan@resnovas.com ,
+ * or see http://www.gnu.org/licenses/gpl-3.0-standalone.html
+ *
+ * DELETING THIS NOTICE AUTOMATICALLY VOIDS YOUR LICENSE - PLEASE SEE THE LICENSE FILE FOR DETAILS
+ * -----
+ * HISTORY:
+ * Date      	By	Comments
+ * ----------	---	---------------------------------------------------------
+ */
+
 /* tslint:disable */
 /* eslint-disable */
 // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
