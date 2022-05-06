dccae76a748fa9c99aa3616d0fe72128e7f5fa14
Jonathan Stevens <jonathan@resnovas.com>
2022-05-06 09:58:42 +0100

    refactor: alxr8 api

--- projects/functions/axlr8/src/Controller.ts
+++ projects/functions/axlr8/src/Controller.ts
@@ -12,29 +12,42 @@
  * Copyright (c) 2022 Resnovas - All Rights Reserved
  * -----
  * LICENSE: GNU General Public License v3.0 or later (GPL-3.0+)
- * 
- * This program has been provided under confidence of the copyright holder and is 
+ *
+ * This program has been provided under confidence of the copyright holder and is
  * licensed for copying, distribution and modification under the terms of
  * the GNU General Public License v3.0 or later (GPL-3.0+) published as the License,
  * or (at your option) any later version of this license.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License v3.0 or later for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License v3.0 or later
  * along with this program. If not, please write to: jonathan@resnovas.com ,
  * or see http://www.gnu.org/licenses/gpl-3.0-standalone.html
- * 
+ *
  * DELETING THIS NOTICE AUTOMATICALLY VOIDS YOUR LICENSE - PLEASE SEE THE LICENSE FILE FOR DETAILS
  * -----
  * HISTORY:
  * Date      	By	Comments
  * ----------	---	---------------------------------------------------------
  */
-import { Controller, Body, Route, Path, Header, Query, Post, Example, TsoaResponse, Res, Response} from 'tsoa';
+import {
+  Controller,
+  Body,
+  Route,
+  Path,
+  Header,
+  Query,
+  Post,
+  Example,
+  TsoaResponse,
+  Res,
+  Response,
+} from 'tsoa';
 import { reportResult, rptFilters, SendAPI } from './util/helpers';
+import { SessionDetails } from './util/reports/EventSessionBooking';
 /**
  * The controller used to create routes.
  * This is used by TSOA to create ./util/routes.ts
@@ -46,98 +59,104 @@ import { reportResult, rptFilters, SendAPI } from './util/helpers';
  */
 @Route('axlr8')
 export class SIACheckController extends Controller {
-    sendAPI = new SendAPI();
+  sendAPI = new SendAPI();
 
-    @Response<reportResult>(422, "Unprocessable Entity", {
-        "result": true,
-        "success": false,
-        "error": "Filter with tag: OSCE_APPLIED_DATE could not be found",
-        "debug": {
-            "rptId": 179,
-            "rptFilters": [
-            {
-                "filter": "OSCE_APPLIED_DATE",
-                "operator": "=",
-                "values": "31/12/2020"
-            }
-            ]
-        }
-    })
-    @Example<reportResult>({
-  "result": true,
-  "success": [
-    {
-      "Account Number": " ",
-      "Client:": "Example",
-      "Venue:": "Example",
-      "Session ID": "0000000",
-      "Sess Id": "00000000",
-      "Customer ID": "null",
-      "No. Required": "1",
-      "Number still required": "1",
-      "No. DNS": "0",
-      "Shift Date": "27/07/9202 09:00",
-      "Start Date/Time": "null",
-      "End Date/Time": "null",
-      "Booking Id": "null",
-      "Booking Status": "No Booking",
-      "No. Confirmed": "null",
-      "Staff Member Confirmed": 0,
-      "Attendee Status": "No Booking",
-      "Declined Status": "Not Set",
-      "DNS": 0,
-      "DNS Reason": "null",
-      "Hour Rate": "null",
-      "Shift Rate": "null",
-      "Staff Rate": "null",
-      "Staff Shift Rate": "null",
-      "Client Hourly Rate": "null",
-      "Full Name": "null",
-      "NI number": "null",
-      "Email": "null",
-      "Operations Manager": "null",
-      "Strikes": "0"
+  @Response<reportResult>(422, 'Unprocessable Entity', {
+    result: true,
+    success: false,
+    error: 'Filter with tag: OSCE_APPLIED_DATE could not be found',
+    debug: {
+      rptId: 179,
+      rptFilters: [
+        {
+          //@ts-ignore
+          filter: 'OSCE_APPLIED_DATE',
+          operator: '=',
+          values: '31/12/2020',
+        },
+      ],
     },
-  ],
-  "error": null,
-  "debug": {
-    "rptId": 1000,
-    "rptFilters": [
+  })
+  @Example<reportResult>({
+    result: true,
+    success: [
       {
-        "filter": "ES_SESSION_NUMBER_STAFF_STILL_REQUIRED",
-        "operator": ">",
-        "values": "0"
+        'Account Number': ' ',
+        'Client:': 'Example',
+        'Venue:': 'Example',
+        'Session ID': '0000000',
+        'Sess Id': '00000000',
+        'Customer ID': 'null',
+        'No. Required': '1',
+        'Number still required': '1',
+        'No. DNS': '0',
+        'Shift Date': '27/07/9202 09:00',
+        'Start Date/Time': 'null',
+        'End Date/Time': 'null',
+        'Booking Id': 'null',
+        'Booking Status': 'No Booking',
+        'No. Confirmed': 'null',
+        'Staff Member Confirmed': 0,
+        'Attendee Status': 'No Booking',
+        'Declined Status': 'Not Set',
+        DNS: 0,
+        'DNS Reason': 'null',
+        'Hour Rate': 'null',
+        'Shift Rate': 'null',
+        'Staff Rate': 'null',
+        'Staff Shift Rate': 'null',
+        'Client Hourly Rate': 'null',
+        'Full Name': 'null',
+        'NI number': 'null',
+        Email: 'null',
+        'Operations Manager': 'null',
+        Strikes: '0',
       },
-      {
-        "filter": "ES_SESSION_START_DATE",
-        "operator": ">",
-        "values": "09/03/2022"
-      }
-    ]
-  }
-})
-    @Post('report/{rptId}')
-    public async getData(
-        @Path() rptId: number,
-        @Query() url: string,
-        @Header() api_user: string,
-        @Header() api_user_pw: string,
-        @Body() rptFilters: rptFilters[],
-        @Res() notFoundResponse: TsoaResponse<422, reportResult>
-    ): Promise<reportResult> {
-        const result = await this.sendAPI.callAPI(
-            url,
-            {
-                rptId,
-                rptFilters: rptFilters
-            },
-            api_user,
-            api_user_pw
-        )
-
-        if (result.success == false) 
-            return notFoundResponse(422, result)
-        else return result
+    ],
+    error: null,
+    debug: {
+      rptId: 1000,
+      rptFilters: [
+        {
+          filter: SessionDetails['Number still required'],
+          operator: '>',
+          values: '0',
+        },
+        {
+          filter: SessionDetails['Shift Date'],
+          operator: '>',
+          values: '09/03/2022',
+        },
+      ],
+    },
+  })
+  @Post('report/{rptId}')
+  public async getData(
+    @Path() rptId: number,
+    @Query() url: string,
+    @Header() api_user: string,
+    @Header() token: string,
+    @Body() rptFilters: rptFilters[],
+    @Res() notFoundResponse: TsoaResponse<422, reportResult>,
+    @Res() unauthorised: TsoaResponse<401, string>
+  ): Promise<reportResult> {
+    let api_pwd: string | undefined;
+    if (api_user == process.env.API_USER) {
+      if (token == process.env.TOKEN) api_pwd = process.env.API_PWD;
     }
-}
+    if (!api_pwd) return unauthorised(401, 'Unauthorised');
 
+    const result = await this.sendAPI.callAPI(
+      url,
+      {
+        rptId,
+        rptFilters: rptFilters,
+      },
+      api_user,
+      api_pwd
+    );
+
+    if (result.success == false) return notFoundResponse(422, result);
+    else return result;
+  }
+}
--- projects/functions/axlr8/src/util/helpers.ts
+++ projects/functions/axlr8/src/util/helpers.ts
@@ -1,40 +1,46 @@
 import crypto from 'crypto';
 // import { filter } from './reports';
 import axios from 'axios';
+import { filter } from './reports';
 
 export interface reportResult {
-  result: boolean,
-  success: { [x: string]: string|number }[] | false,
-  error: null | string,
-  debug: postDataString
+  result: boolean;
+  success: { [x: string]: string | number }[] | false;
+  error: null | string;
+  debug: postDataString;
 }
 export interface postDataString {
-    rptId: number;
-    rptFilters: {
-      filter: string;
-      operator: string;
-      values: string;
-    }[];
-  }
+  rptId: number;
+  rptFilters: {
+    filter: filter;
+    operator: string;
+    values: string;
+  }[];
+}
 
 export interface postData {
-    rptId: number;
-    rptFilters: rptFilters[];
-  }
+  rptId: number;
+  rptFilters: rptFilters[];
+}
 
 export interface rptFilters {
-      filter: string;
-      operator: string;
-      values: string;
-    }
+  filter: filter;
+  operator: string;
+  values: string;
+}
 
 export class SendAPI {
   private func = 'GetReportData';
 
-  public async callAPI(url: string, postdata: postData, api_user: string, api_user_pw: string): Promise<reportResult> {
+  public async callAPI(
+    url: string,
+    postdata: postData,
+    api_user: string,
+    api_user_pw: string
+  ): Promise<reportResult> {
     const timestamp = new Date().getTime().toString();
 
-    const form  = new URLSearchParams();
+    const form = new URLSearchParams();
     form.append('func', this.func);
     form.append('reqData', JSON.stringify(postdata));
     form.append('username', api_user);
@@ -42,35 +48,32 @@ export class SendAPI {
     form.append(
       'hash',
       this.getHash(
-        [
-          url,
-          api_user,
-          this.func,
-          JSON.stringify(postdata),
-          timestamp,
-        ],
-      api_user_pw
+        [url, api_user, this.func, JSON.stringify(postdata), timestamp],
+        api_user_pw
       )
-    )
-    const result = await axios.post(url, form, {
-      "timeout": 100000,
-      "headers": {
-        'Accept': '*.*',
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    }).catch(err => {
-      console.log(err)
-      throw err
-    })
+    );
+    const result = await axios
+      .post(url, form, {
+        timeout: 100000,
+        headers: {
+          Accept: '*.*',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+      .catch((err) => {
+        console.log(err);
+        throw err;
+      });
     // console.log(result.status)
-    return result.data as reportResult
+    return result.data as reportResult;
   }
 
   // use array to generate hash
-  private getHash(
-    parts: any[],
-    passcode: string
-  ): string {
-    return crypto.createHmac('sha256', Buffer.from(passcode, 'utf-8')).update(parts.join("")).digest().toString('base64')
+  private getHash(parts: any[], passcode: string): string {
+    return crypto
+      .createHmac('sha256', Buffer.from(passcode, 'utf-8'))
+      .update(parts.join(''))
+      .digest()
+      .toString('base64');
   }
 }
